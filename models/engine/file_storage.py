59520de132fc716fd9b7f4af42402b708edf6eee
popeopzy <popeopzy@gmail.com>
2024-03-20 13:58:15 +0100

    Update FileStorage: models/engine/file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,26 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage
+        #return FileStorage.__objects
+        Args:
+            cls (class, optional): If specified, filters the result to include
+                only objects of the specified class.
+
+        Returns:
+            dict: A dictionary containing objects in storage.
+        """
+        if cls:
+            if isinstance(cls, str):
+                cls = globals().get(cls)
+            if cls and issubclass(cls, BaseModel):
+                 cls_dict = {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+                 return cls_dict
         return FileStorage.__objects
+
+
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +65,20 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+        
+        def delete(self, obj=None)
+             """
+             Delete obj from __objects if it’s inside - if obj is equal to None,
+             the method should not do anything
+             """
+             if obj is None:
+                 return
+             obj_to_del = f"{obj.__class__.__name__}.{obj.id}"
+
+             try:
+                 del FileStorage.__objects[obj_to_del]
+            except AttributeError:
+                pass
+            except KeyboardInterrupt:
+                pass
