30f725c4f5da1e84dc92befab48346c8eddc4547
popeopzy <popeopzy@gmail.com>
2024-03-20 21:34:17 +0100

    Error correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+
 
 
 class FileStorage:
@@ -67,18 +69,17 @@ class FileStorage:
             pass
 
         
-        def delete(self, obj=None)
-             """
-             Delete obj from __objects if it’s inside - if obj is equal to None,
-             the method should not do anything
-             """
-             if obj is None:
-                 return
-             obj_to_del = f"{obj.__class__.__name__}.{obj.id}"
-
-             try:
-                 del FileStorage.__objects[obj_to_del]
+        def delete(self, obj=None):
+            """
+            Delete obj from __objects if it’s inside - if obj is equal to None,the method should not do anything
+            """
+            if obj is None:
+                return
+            obj_to_del = f"{obj.__class__.__name__}.{obj.id}"
+            try:
+                del FileStorage.__objects[obj_to_del]
             except AttributeError:
                 pass
             except KeyboardInterrupt:
                 pass
+            
